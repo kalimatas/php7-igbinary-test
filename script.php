eb90fd35898eb9525b7af639e565d8733b7be0be
Alexander Guz <kalimatas@gmail.com>
2016-10-16 17:42:02 +0200

    See Segmentation fault error

--- script.php
+++ script.php
@@ -2,7 +2,8 @@
 
 $result = [];
 
-for ($i = 0; $i < 40; $i++) {
+for ($i = 0; $i <= 40; $i++) {
+  if ($i > 0 && $i % 10 == 0) echo $i . PHP_EOL;
   $inner = new stdClass;
   $inner->a = $i % 25;
   $result[0][$inner->a][$i] = $inner;
